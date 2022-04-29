3faab715da283aff825a40003707e8c272a7f21e
puskottampandey <puskottam.pandey@gmail.com>
2022-04-29 08:14:38 +0545

    colochange

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,43 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    ),
-  );
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  SplashScreenState createState() => SplashScreenState();
-}
-
-class SplashScreenState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        const Duration(seconds: 2),
-        () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const Homepage())));
-  }
-
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue.shade900,
-    );
-  }
-}
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -53,20 +14,14 @@ class Homepage extends StatelessWidget {
           child: ListView(
             padding: EdgeInsets.only(),
             children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue.shade900),
-              child: Text(
-                  "Questionbank",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-             
-                ),
+              const DrawerHeader(
+                decoration: BoxDecoration(color: (Colors.blue)),
+                child: Text("QuestionBank"),
               ),
               ListTile(
                 leading: Icon(Icons.notification_add),
                 title: Text("Notification"),
-                onTap: (){
-                  Navigator.push(context,)
-                },
+                onTap: () {},
               ),
               ListTile(
                 leading: Icon(Icons.mail),
