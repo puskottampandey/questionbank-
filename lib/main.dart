329ebc1e54129b55e2a57164ed7f94c86d6caf89
puskottampandey <puskottam.pandey@gmail.com>
2022-05-01 16:33:11 +0545

    Made drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:questionbank/Homepage.dart';
 
 void main() {
   runApp(
@@ -36,68 +37,5 @@ class SplashScreenState extends State<HomePage> {
     return Container(
       color: Colors.blue.shade900,
     );
-  }
-}
-
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: (Colors.blue.shade900),
-          title: const Text("Questionbank"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.only(),
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue.shade900),
-              child: Text(
-                  "Questionbank",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-             
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.notification_add),
-                title: Text("Notification"),
-                onTap: (){
-                  Navigator.push(context,)
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.mail),
-                title: Text("Mail"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Setting"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.send),
-                title: Text("Send Feedback "),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.rate_review),
-                title: Text("Rate us "),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          ),
-        ));
   }
 }
